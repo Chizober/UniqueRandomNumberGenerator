25396d7dad00756f7eb49c834cd3f13b974d5b49
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2023-01-26 11:50:37 +0100

    Add files via upload

---
+++ RandNumber/RandNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RandNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Enumerable.Range(0, 27).ToArray();
+            var randNum = new Random();
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int randomIndex = randNum.Next(nums.Length);
+                int temp = nums[randomIndex];
+                nums[randomIndex] = nums[i];
+                nums[i] = temp;
+            }
+
+            for (int i = 0; i < nums.Length; ++i)
+                Console.WriteLine(nums[i]);
+        }
+    }
+}
